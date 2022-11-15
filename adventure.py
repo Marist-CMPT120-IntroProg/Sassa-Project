82a2ea13a265e0fd37f481c8942c51ec39786543
lukesassa <112555397+lukesassa@users.noreply.github.com>
2022-11-15 08:25:52 -0500

    Proj3 Comm1(Made List of Dicts, yet to implement)

--- adventure.py
+++ adventure.py
@@ -14,19 +14,28 @@ def main():
     list1 = []
     #adding the first command to the empty list
     list1.append(input("Enter a valid command:"))
-    #including the given name in within the text as a float
-    bigbrook = (f"It's been a long day, {name}. You've had fun at Big Brook Park, with a delicious late afternoon picnic to top it off, but now it's time to make the 7 mile bike ride home. The only issue is that you will need energy to complete the voyage! For this reason, you must stop off at the local coffee shop before they close for the night, and then you may continue with your journey.")
-    detail1 = (f"As you begin to leave the park, it's hard not to reflect upon the beauty of the lush meadows and colorful flowers surrounding you.")
-    def location1():
+    #creating a list of dictionaries to store each location and its accompanying location data
+    locationsList = [{"location": "Big Brook Park", "summary": f"It's been a long day, {name}. You've had fun at Big Brook Park, with a delicious late afternoon picnic to top it off, but now it's time to make the 7 mile bike ride home. The only issue is that you will need energy to complete the voyage! For this reason, you must stop off at the local coffee shop before they close for the night, and then you may continue with your journey.", "details": f"As you begin to leave the park, it's hard not to reflect upon the beauty of the lush meadows and colorful flowers surrounding you.", "was_visited": False},
+    {"location": "Coffee Shop", "summary": "After exiting the park and biking a half mile up the road, you've arrived at the coffee shop in just the nick of time! After ordering a large iced coffee, its time to hit the road on the way to the bike trail.", "details": "The friendly barista wishes you the best of luck as you make your way out the door and back onto your bike.", "was_visited": False},
+    {"location": "Route 79", "summary": f"To get to the start of the bike trail, you have no choice but to bike along a busy stretch of Route 79. Be careful {name}! It's almost dusk, so visibility is decreasing. There's no sidewalk, as you'll be biking past nothing but farms, so make sure to stay within the shoulder of the road!", "details": f"Beware, {name}: this part of the trek is a little scary, as you can literally feel the cars rushing on by right next to you!", "was_visited": False},
+    {"location": "Bike Hub", "summary": f"At long last, you've arrived at the trailhead! Congrats {name}! It's time to complete the bulk of your journey! But wait! One of the tires on your bike feels a little light on air. Luckily, The Bicycle Hub bike shop is located right next to the trailhead! Time to fill up some air in that tire and continue on your journey.", "details": f"The shop is closed, but fortunately you find an air pump laying in the parking lot.", "was_visited": False},
+    {"location": "Airport", "summary": "Now that your tires are all filled up with air, you begin biking along the paved path in earnest. About a mile into the trail, one of your favorite sunset-watching spots appears on your left adjacent to the trail- the Abandoned Marlboro Airport. And perfect timing, as golden hour has just begun! A brief pitstop on the abandoned tarmac won't hurt, but after snapping a few photos, it's time to get back on the trail!", "details": "Still, you can't help but think about how scenic the sun setting over the large clearing and old tarmac is, even though it's time to move along.", "was_visited": False},
+    {"location": "Cemetary", "summary": "As you continue your ride, the trail winds up cutting directly through a creepy cemetary with a large Mausoleum. Off in the distance, you notice a cult-like group of people in red robes gathered around in a circle. As tempting as it is to check it out, you must continue onward and avoid leaving the trail!", "details": "For a split second, you pull out your phone to record the strange gathering, but then notice the people turning in your direction, prompting you to speed off.", "was_visited": False},
+    {"location": "Dirt Bike Trails", "summary": "Biking fast and furious and feeling slightly terrified after seeing such a weird sighting, you begin to realize that daylight is fading quickly. Luckily, a cut-through exists in the form of dirt-bike trails, which run off of the paved bike trail. The dirt bike trails cut off a significant portion of the journey before rejoining the paved trail later down the line, so it's time to take a detour!", "details": "You feel relieved that your tires are now fully inflated, as they're about to take a beating on the rough terrain of the dirt bike trails.", "was_visited": False}, 
+    {"location": "The Bog", "summary": f"After bumping your way along the rugged dirt bike trails, you finally reemerge onto the glorious pavement. Nice job {name}! But the tricky terrain doesn't end there – mud from the polluted Burnt Fly Bog superfund site has leeched onto a low-lying portion of the bike trail after a recent storm, causing treacherous and slippery conditions. For this portion of the trail, you must walk your bike as a precaution against the dangerous conditions.", "details": "While walking your bike, your white shoes become completely soiled in the muck, but it's better than trying to ride the bike through the muck and risking injury.", "was_visited": False},
+    {"location": "Substation", "summary": "As you move past the bog and mount back onto your bicycle, you begin to hear the hum of the Freneau electrical substation off in the near distance. You know this means you're in the homestretch! Keep biking as fast as you can – the bats are beginning to emerge!", "details": "The electrical substation is surrounded by a 10-foot high barbed wire fence and high voltage signs, which only add to the eery feel of the bike trail at dusk.", "was_visited": False},
+    {"location": "Shack", "summary": "As you near the end of the bike trail, there's one last landmark to pass! On your righthand side, you see it coming closer and closer: The creepy abandoned shack! As the purple graffiti of the shack gets closer and closer, a strange man emerges and runs towards the trail! Luckily you're able to slip past him just in time and make your way home!", "details": "The man is old with white hair, but you end up biking past him so fast that you're not able to get a good look at him or his face.", "was_visited": False}]
+    def first_location():
         while count == 0:
             #making sure the command is interpreted as a string and eliminating case-sensitive
             if str.lower(list1[0]) == "north":
-                location = bigbrook
+                location = locationsList[0]["summary"]
                 print(location)
                 #Revealing deatils by prompting the user to examine the location (as per commit 5)
                 enter1 = input("Press Enter to Examine Location")
                 print(enter1)
-                print(detail1)
+                print(locationsList[0]["details"])
+                locationsList[0]["was_visited"] = True
                 break
             #making sure the command is interpreted as a string and eliminating case-sensitive
             elif str.lower(list1[0]) == "south":
@@ -44,14 +53,12 @@ def main():
                 print("You've entered an invalid command! The list of valid commands is: North, South, Help, and Quit")
                 #giving the user a chance to re-enter a valid command
                 list1[0] = str.lower(input("Enter a valid command:"))
-    location1()
+    first_location()
     #increase location count by 1 after the first stop
     count = count + 1
     #replacing command in the list with a new command to be interpreted by the next while-loop
     list1[0] = input("Enter a valid command:")
-    coffee = (f"After exiting the park and biking a half mile up the road, you've arrived at the coffee shop in just the nick of time! After ordering a large iced coffee, its time to hit the road on the way to the bike trail.")
-    detail2 = (f"The friendly barista wishes you the best of luck as you make your way out the door and back onto your bike.")
-    def location2():
+    def second_location():
         while count == 1.0:
             #making sure the command is interpreted as a string and eliminating case-sensitive
             if str.lower(list1[0]) == "north":
@@ -83,14 +90,12 @@ def main():
                 print("You've entered an invalid command! The list of valid commands is: North, South, Help, and Quit")
                 #giving the user a chance to re-enter a valid command
                 list1[0] = str.lower(input("Enter a valid command:"))
-    location2()
+    second_location()
     #increase location count by 1 after the next stop
     count = count + 1
     #replacing command in the list with a new command to be interpreted by the next while-loop
     list1[0] = input("Enter a valid command:")
-    route = f"To get to the start of the bike trail, you have no choice but to bike along a busy stretch of Route 79. Be careful {name}! It's almost dusk, so visibility is decreasing. There's no sidewalk, as you'll be biking past nothing but farms, so make sure to stay within the shoulder of the road!"
-    detail3 = f"Beware, {name}: this part of the trek is a little scary, as you can literally feel the cars rushing on by right next to you!"
-    def location3():
+    def third_location():
         while count == 2.0:
             #making sure the command is interpreted as a string and eliminating case-sensitive
             if str.lower(list1[0]) == "north":
@@ -122,14 +127,12 @@ def main():
                 print("You've entered an invalid command! The list of valid commands is: North, South, Help, and Quit")
                 #giving the user a chance to re-enter a valid command
                 list1[0] = str.lower(input("Enter a valid command:"))
-    location3()
+    third_location()
     #increase location count by 1 after the next stop
     count = count + 1
     #replacing command in the list with a new command to be interpreted by the next while-loop
     list1[0] = input("Enter a valid command:")
-    bikehub = f"At long last, you've arrived at the trailhead! Congrats {name}! It's time to complete the bulk of your journey! But wait! One of the tires on your bike feels a little light on air. Luckily, The Bicycle Hub bike shop is located right next to the trailhead! Time to fill up some air in that tire and continue on your journey."
-    detail4 = f"The shop is closed, but fortunately you find an air pump laying in the parking lot."
-    def location4():
+    def fourth_location():
         while count == 3.0:
             #making sure the command is interpreted as a string and eliminating case-sensitive
             if str.lower(list1[0]) == "north":
@@ -161,14 +164,12 @@ def main():
                 print("You've entered an invalid command! The list of valid commands is: North, South, Help, and Quit")
                 #giving the user a chance to re-enter a valid command
                 list1[0] = str.lower(input("Enter a valid command:"))
-    location4()
+    fourth_location()
     #increase location count by 1 after the next stop
     count = count + 1
     #replacing command in the list with a new command to be interpreted by the next while-loop
     list1[0] = input("Enter a valid command:")
-    airport = (f"Now that your tires are all filled up with air, you begin biking along the paved path in earnest. About a mile into the trail, one of your favorite sunset-watching spots appears on your left adjacent to the trail- the Abandoned Marlboro Airport. And perfect timing, as golden hour has just begun! A brief pitstop on the abandoned tarmac won't hurt, but after snapping a few photos, it's time to get back on the trail!")
-    detail5 = (f"Still, you can't help but think about how scenic the sun setting over the large clearing and old tarmac is, even though it's time to move along.")
-    def location5():
+    def fifth_location():
         while count == 4.0:
             #making sure the command is interpreted as a string and eliminating case-sensitive
             if str.lower(list1[0]) == "north":
@@ -200,14 +201,12 @@ def main():
                 print("You've entered an invalid command! The list of valid commands is: North, South, Help, and Quit")
                 #giving the user a chance to re-enter a valid command
                 list1[0] = str.lower(input("Enter a valid command:"))
-    location5()
+    fifth_location()
     #increase location count by 1 after the next stop
     count = count + 1
     #replacing command in the list with a new command to be interpreted by the next while-loop
     list1[0] = input("Enter a valid command:")
-    cemetary = (f"As you continue your ride, the trail winds up cutting directly through a creepy cemetary with a large Mausoleum. Off in the distance, you notice a cult-like group of people in red robes gathered around in a circle. As tempting as it is to check it out, you must continue onward and avoid leaving the trail!")
-    detail6 = (f"For a split second, you pull out your phone to record the strange gathering, but then notice the people turning in your direction, prompting you to speed off.")
-    def location6():
+    def sixth_location():
         while count == 5.0:
             #making sure the command is interpreted as a string and eliminating case-sensitive
             if str.lower(list1[0]) == "north":
@@ -239,14 +238,12 @@ def main():
                 print("You've entered an invalid command! The list of valid commands is: North, South, Help, and Quit")
                 #giving the user a chance to re-enter a valid command
                 list1[0] = str.lower(input("Enter a valid command:"))
-    location6()
+    sixth_location()
     #increase location count by 1 after the next stop
     count = count + 1
     #replacing command in the list with a new command to be interpreted by the next while-loop
     list1[0] = input("Enter a valid command:")
-    dirt = (f"Biking fast and furious and feeling slightly terrified after seeing such a weird sighting, you begin to realize that daylight is fading quickly. Luckily, a cut-through exists in the form of dirt-bike trails, which run off of the paved bike trail. The dirt bike trails cut off a significant portion of the journey before rejoining the paved trail later down the line, so it's time to take a detour!")
-    detail7 = (f"You feel relieved that your tires are now fully inflated, as they're about to take a beating on the rough terrain of the dirt bike trails.")
-    def location7():
+    def seventh_location():
         while count == 6.0:
             #making sure the command is interpreted as a string and eliminating case-sensitive
             if str.lower(list1[0]) == "north":
@@ -278,14 +275,12 @@ def main():
                 print("You've entered an invalid command! The list of valid commands is: North, South, Help, and Quit")
                 #giving the user a chance to re-enter a valid command
                 list1[0] = str.lower(input("Enter a valid command:"))
-    location7()
+    seventh_location()
     #increase location count by 1 after the next stop
     count = count + 1
     #replacing command in the list with a new command to be interpreted by the next while-loop
     list1[0] = input("Enter a valid command:")
-    bog = (f"After bumping your way along the rugged dirt bike trails, you finally reemerge onto the glorious pavement. Nice job {name}! But the tricky terrain doesn't end there – mud from the polluted Burnt Fly Bog superfund site has leeched onto a low-lying portion of the bike trail after a recent storm, causing treacherous and slippery conditions. For this portion of the trail, you must walk your bike as a precaution against the dangerous conditions.")
-    detail8 = (f"While walking your bike, your white shoes become completely soiled in the muck, but it's better than trying to ride the bike through the muck and risking injury.")
-    def location8():
+    def eighth_location():
         while count == 7.0:
             #making sure the command is interpreted as a string and eliminating case-sensitive
             if str.lower(list1[0]) == "north":
@@ -317,14 +312,12 @@ def main():
                 print("You've entered an invalid command! The list of valid commands is: North, South, Help, and Quit")
                 #giving the user a chance to re-enter a valid command
                 list1[0] = str.lower(input("Enter a valid command:"))
-    location8()
+    eighth_location()
     #increase location count by 1 after the next stop
     count = count + 1 
     #replacing command in the list with a new command to be interpreted by the next while-loop 
     list1[0] = input("Enter a valid command:")  
-    substation = (f"As you move past the bog and mount back onto your bicycle, you begin to hear the hum of the Freneau electrical substation off in the near distance. You know this means you're in the homestretch! Keep biking as fast as you can – the bats are beginning to emerge!")
-    detail9 = (f"The electrical substation is surrounded by a 10-foot high barbed wire fence and high voltage signs, which only add to the eery feel of the bike trail at dusk.")
-    def location9():
+    def ninth_location():
         while count == 8.0:
             #making sure the command is interpreted as a string and eliminating case-sensitive
             if str.lower(list1[0]) == "north":
@@ -356,14 +349,12 @@ def main():
                 print("You've entered an invalid command! The list of valid commands is: North, South, Help, and Quit")
                 #giving the user a chance to re-enter a valid command
                 list1[0] = str.lower(input("Enter a valid command:"))
-    location9()
+    ninth_location()
     #increase location count by 1 after the next stop
     count = count + 1
     #replacing command in the list with a new command to be interpreted by the next while-loop
     list1[0] = input("Enter a valid command:")
-    shack = (f"As you near the end of the bike trail, there's one last landmark to pass! On your righthand side, you see it coming closer and closer: The creepy abandoned shack! As the purple graffiti of the shack gets closer and closer, a strange man emerges and runs towards the trail! Luckily you're able to slip past him just in time and make your way home!")
-    detail10 = (f"The man is old with white hair, but you end up biking past him so fast that you're not able to get a good look at him or his face.")
-    def location10():
+    def tenth_location():
         while count == 9.0:
             #making sure the command is interpreted as a string and eliminating case-sensitive
             if str.lower(list1[0]) == "north":
@@ -395,7 +386,7 @@ def main():
                 print("You've entered an invalid command! The list of valid commands is: North, South, Help, and Quit")
                 #giving the user a chance to re-enter a valid command
                 list1[0] = str.lower(input("Enter a valid command:"))
-    location10()
+    tenth_location()
     #increase location count by 1 after the next stop
     count = count + 1
     def ending():
